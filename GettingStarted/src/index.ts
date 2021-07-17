e8e85651a13855d26ebd167d097a7b0a507788df
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-18 01:45:32 +0600

    Make same as tutorial

--- GettingStarted/src/index.ts
+++ GettingStarted/src/index.ts
@@ -3,10 +3,9 @@ import * as THREE from "three";
 
 let canvas = document.querySelector("#c") as HTMLCanvasElement;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, 2, 0.1, 5);
 
 const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
 
 function makeCube(x:number,y:number,z:number) {
     const geometry = new THREE.BoxGeometry();
