c2715b147012be6d8f5d3e9097e1fa92f2420069
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-18 13:55:01 +0600

    Changing to tutorial Style

--- GettingStarted/src/index.ts
+++ GettingStarted/src/index.ts
@@ -1,74 +1,42 @@
 import * as THREE from "three";
+import {Mesh} from "three";
 
 
 let canvas = document.querySelector("#c") as HTMLCanvasElement;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, 2, 0.1, 5);
+
+
 
 const renderer = new THREE.WebGLRenderer({canvas});
 
-function makeCube(x:number,y:number,z:number) {
-    const geometry = new THREE.BoxGeometry();
+const camera = new THREE.PerspectiveCamera( 40, 2, 0.1, 1000);
+camera.position.set(0,50,0);
+camera.up.set(0,0,1);
+camera.lookAt(0,0,0);
 
-    const material = new THREE.MeshPhysicalMaterial({color: 0x44aa88});
-    let cube = new THREE.Mesh(geometry, material);
-    setMeshPosition(cube,x,y,z);
-    return cube;
+const scene = new THREE.Scene();
+
+{
+
+    let light = new THREE.PointLight("0xFFFFFF",3);
+    scene.add(light);
 }
 
+const objects : Mesh[] =[];
+
+const sphereGeometry = new THREE.SphereGeometry(1,6,6);
 
 
+const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+const sunMesh = new THREE.Mesh(sphereGeometry,sunMaterial);
+sunMesh.scale.set(5,5,5);
+scene.add(sunMesh);
+objects.push(sunMesh);
 
-
-
-
-function  setMeshPosition(mesh: THREE.Mesh, x:number,y:number,z:number){
-    mesh.position.x = x;
-    mesh.position.y = y;
-    mesh.position.z = z;
-}
-
-const cube1 = makeCube(3,1,1);
-const cube2 = makeCube(1,1,3);
-const cube3 = makeCube(1,3,1);
-
-cube2.material.color.setHex(0xFF0000);
-cube3.material.color.setHex(0x00FF00)
-
-scene.add( cube1,cube2,cube3 );
-let light = new THREE.DirectionalLight("0xFFFFFF",1);
-
-scene.add(light);
-camera.position.z = 5;
-camera.position.x = 2;
-camera.position.y = 1.5;
-
-
-light.position.set(camera.position.x,camera.position.y,camera.position.z);
-
-function animate() {
-	requestAnimationFrame( animate );
-
-    cube1.rotation.x += .01;
-    cube1.rotation.y -= .04;
-
-
-    cube2.rotation.x -= .09;
-    cube2.rotation.y += .01;
-
-    cube3.rotation.x -= .03;
-    cube3.rotation.y += .01;
-
-    if( resizeRendererToDisplaySize(renderer) ){
-        const canvas = renderer.domElement;
-        camera.aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.updateProjectionMatrix();
-    }
-
-
-	renderer.render( scene, camera );
-}
-
+const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
+const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+earthMesh.position.x = 10;
+scene.add(earthMesh);
+objects.push(earthMesh);
 
 
 function resizeRendererToDisplaySize(renderer:THREE.Renderer){
@@ -84,7 +52,24 @@ function resizeRendererToDisplaySize(renderer:THREE.Renderer){
 }
 
 
+function render(time:number) {
+    time *= 0.001;
 
-animate();
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    objects.forEach((obj) => {
+        obj.rotation.y = time;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
 
 
